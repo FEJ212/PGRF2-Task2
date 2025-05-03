38bdaefbc5eaeff58eda6c49715500661765e473
fejteon1 <fejteon1@uhk.cz>
2025-05-03 17:31:03 +0200

    todo

---
+++ src/main/java/pgrf2/engine/todo.java
@@ -0,0 +1,18 @@
+package pgrf2.engine;
+
+public class todo {
+    //TODO: Skybox
+    //TODO: Přehrávání hudby
+    //TODO: Animace disku
+    //TODO: Animace knoflíků
+    //TODO: vymodelování ramena s jehlou
+    //TODO: animace ramena s jehlou
+    //TODO: ovládání rychlosti přehrávání
+    //TODO: ovládání hlasitosti
+    //TODO: UI
+    //TODO: vlákna pro ovládání i po kliknutí na UI
+    //TODO: nápověda v UI
+    //TODO: výběr hudby z nabídky
+    //TODO: nahrání vlastní hudby
+    //TODO: vlastní obrázky k hudbě
+}
